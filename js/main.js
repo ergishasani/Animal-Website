d65e2d9f017806153359ba8f28281ef073bf56e2
Ergis <gisihasani95@gmail.com>
2024-06-06 15:15:48 +0200

    Updates

--- js/main.js
+++ js/main.js
@@ -3,8 +3,10 @@
 document.addEventListener('DOMContentLoaded', () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('nav');
+    const logo = document.querySelector('header a img');
 
     burger.addEventListener('click', () => {
         nav.classList.toggle('active');
+        logo.classList.toggle('logo');
     });
 });
