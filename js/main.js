029a10a6f35f6cdde6683c0c22d54b533cfe499a
Ergis <gisihasani95@gmail.com>
2024-06-06 16:56:58 +0200

    Updates

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-// JavaScript to toggle the burger menu
-
 document.addEventListener('DOMContentLoaded', () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('nav');
